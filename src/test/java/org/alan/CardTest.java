bfa797a26eaa6ae31261ba8cc0abd5a718144a01
alankm194 <alankm194@gmail.com>
2023-03-02 23:47:57 +0000

    test: add tests for getting faces values from card

--- src/test/java/org/alan/CardTest.java
+++ src/test/java/org/alan/CardTest.java
@@ -1,6 +1,5 @@
 package org.alan;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -8,16 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CardTest {
 
-    @Test
-    void testGetFaceValue() {
-        Card ace = new Card(FaceValues.ACE, SuitValues.SPADES);
-        assertEquals(FaceValues.ACE, ace.getFaceValue());
+    @ParameterizedTest
+    @CsvSource({"ACE", "KING", "TWO", "THREE", "SEVEN", "NINE", "TEN", "JACK"})
+    void testGetFaceValue(String input) {
+        Card card = new Card(FaceValues.valueOf(input), SuitValues.SPADES);
+        assertEquals(FaceValues.valueOf(input), card.getFaceValue());
     }
 
     @ParameterizedTest
     @CsvSource({"DIAMOND", "CLUB", "HEARTS", "SPADES",})
     void testGetSuitValue(String input) {
-        Card ace = new Card(FaceValues.ACE, SuitValues.valueOf(input));
-        assertEquals(SuitValues.valueOf(input), ace.getSuitValue());
+        Card card = new Card(FaceValues.ACE, SuitValues.valueOf(input));
+        assertEquals(SuitValues.valueOf(input), card.getSuitValue());
     }
 }
