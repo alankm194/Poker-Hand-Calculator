05b781fb290c889206d74ca4c8d92809371ae4a5
alankm194 <alankm194@gmail.com>
2023-03-03 00:44:15 +0000

    refactor: shorten code in HandTest for whenHandIsGivenListOver5Cards_ThrowIllegalArgumentException

--- src/test/java/org/alan/HandTest.java
+++ src/test/java/org/alan/HandTest.java
@@ -3,6 +3,7 @@ package org.alan;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,19 +28,16 @@ class HandTest {
     @DisplayName("Throw illegalArgumentException when trying to set a Hand with more than 5 cards")
     void whenHandIsGivenListOver5Cards_ThrowIllegalArgumentException() {
         var hand = new Hand();
-        var expectedList = List.of(
-                new Card(FaceValues.ACE, SuitValues.SPADES),
-                new Card(FaceValues.ACE, SuitValues.SPADES),
-                new Card(FaceValues.FOUR, SuitValues.HEARTS),
-                new Card(FaceValues.KING, SuitValues.CLUB),
-                new Card(FaceValues.JACK, SuitValues.CLUB),
-                new Card(FaceValues.THREE, SuitValues.DIAMOND));
+        var cardsList = new ArrayList<Card>();
+        for (int i = 0; i < 6; i++) {
+            cardsList.add(new Card(FaceValues.ACE, SuitValues.SPADES));
+        }
+
         IllegalArgumentException thrown = assertThrows(
                 IllegalArgumentException.class,
-                () -> hand.setHand(expectedList),
+                () -> hand.setHand(cardsList),
                 "Hand cannot set hand of more than 5 cards."
         );
-
         assertEquals("A legal hand must be 5 cards", thrown.getMessage());
     }
 }
