9932b7f2ad48157dc792aae2494bb95309de469c
alankm194 <alankm194@gmail.com>
2023-03-03 23:52:31 +0000

    feat: implement comparable for Card
    - override compareTo, equals and hashcode
    - fix missing maven dependency junit params

--- src/main/java/org/alan/Card.java
+++ src/main/java/org/alan/Card.java
@@ -1,10 +1,14 @@
 package org.alan;
 
-public class Card {
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Card implements Comparable<Card> {
 
     private FaceValues face;
 
     private SuitValues suit;
+
     public Card(FaceValues face, SuitValues suit) {
         this.face = face;
         this.suit = suit;
@@ -17,4 +21,32 @@ public class Card {
     public FaceValues getFaceValue() {
         return face;
     }
+
+    @Override
+    public String toString() {
+        return String.format("face: %s suit: %s", face, suit);
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return face == card.face && suit == card.suit;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(face, suit);
+    }
+
+    @Override
+    public int compareTo(Card o) {
+        return Comparator
+                .comparing(Card::getFaceValue, Comparator.comparingInt(FaceValues::getValue))
+                .thenComparing(Card::getSuitValue, Comparator.comparingInt(SuitValues::getValue))
+                .compare(this, o);
+    }
+
 }
--- src/test/java/org/alan/CardTest.java
+++ src/test/java/org/alan/CardTest.java
@@ -1,9 +1,15 @@
 package org.alan;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CardTest {
 
@@ -19,5 +25,68 @@ class CardTest {
     void testGetSuitValue(String input) {
         Card card = new Card(FaceValues.ACE, SuitValues.valueOf(input));
         assertEquals(SuitValues.valueOf(input), card.getSuitValue());
+    }
+
+    @Test
+    void testEqualsReflexive() {
+        Card a = new Card(FaceValues.KING, SuitValues.SPADES);
+        assertTrue(a.equals(a));
+    }
+
+    @Test
+    void testEqualsSymmetric() {
+        Card a = new Card(FaceValues.KING, SuitValues.SPADES);
+        Card b = new Card(FaceValues.KING, SuitValues.SPADES);
+        assertTrue(a.equals(b));
+    }
+
+    @Test
+    void testEqualsTransitive() {
+        Card a = new Card(FaceValues.KING, SuitValues.SPADES);
+        Card b = new Card(FaceValues.KING, SuitValues.SPADES);
+        Card c = new Card(FaceValues.KING, SuitValues.SPADES);
+        assertTrue(a.equals(b));
+        assertTrue(b.equals(c));
+        assertTrue(a.equals(c));
+    }
+
+    @Test
+    void testHashCodeProduceSameHash() {
+        var cardList = List.of(new Card(FaceValues.FOUR, SuitValues.CLUB),
+                new Card(FaceValues.TWO, SuitValues.DIAMOND),
+                new Card(FaceValues.ACE, SuitValues.SPADES),
+                new Card(FaceValues.FOUR, SuitValues.HEARTS));
+
+        var hashCodeList = cardList
+                .stream()
+                .map(Objects::hashCode)
+                .collect(Collectors.toList());
+
+        assertEquals(hashCodeList, cardList.stream().map(Card::hashCode).toList());
+    }
+
+    @Test
+    void testCompareToGreaterThan() {
+        Card greater = new Card(FaceValues.ACE, SuitValues.SPADES);
+        Card lesser = new Card(FaceValues.KING, SuitValues.DIAMOND);
+
+        assertEquals(1, greater.compareTo(lesser));
+    }
+
+
+    @Test
+    void testCompareToLesserThan() {
+        Card greater = new Card(FaceValues.ACE, SuitValues.SPADES);
+        Card lesser = new Card(FaceValues.KING, SuitValues.DIAMOND);
+
+        assertEquals(-1, lesser.compareTo(greater));
+    }
+
+    @Test
+    void testCompareToEquals() {
+        Card same = new Card(FaceValues.ACE, SuitValues.SPADES);
+        Card same2 = new Card(FaceValues.ACE, SuitValues.SPADES);
+
+        assertEquals(0, same.compareTo(same2));
     }
 }
