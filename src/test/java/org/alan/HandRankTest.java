8039486622b9784154455bb3e6c31af19e58df71
alankm194 <alankm194@gmail.com>
2023-03-04 18:09:59 +0000

    test: add more tests to handRank enum

--- src/test/java/org/alan/HandRankTest.java
+++ src/test/java/org/alan/HandRankTest.java
@@ -1,13 +1,15 @@
 package org.alan;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HandRankTest {
-    @Test
-    void testGetRank() {
-        assertEquals(6, HandRank.FULL_HOUSE.getRank());
+    @ParameterizedTest
+    @CsvFileSource(resources = "/handRankTest.csv", numLinesToSkip = 1)
+    void testGetRank(String hand, int expectedRank) {
+        assertEquals(expectedRank, HandRank.valueOf(hand).getRank());
     }
 
 }
