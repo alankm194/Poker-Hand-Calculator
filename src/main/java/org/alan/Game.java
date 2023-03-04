288b044de19addb9c14740942c496ff4a4e2e8f1
alankm194 <alankm194@gmail.com>
2023-03-04 23:09:07 +0000

    feat: add feature to detect if hand is straight flush or 4 of a kind

--- src/main/java/org/alan/Game.java
+++ src/main/java/org/alan/Game.java
@@ -1,9 +1,14 @@
 package org.alan;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Game {
 
     private final Hand player1;
     private final Hand player2;
+
+    private HandRank handRank;
 
     public Game(Hand player1, Hand player2) {
         this.player1 = player1;
@@ -17,4 +22,40 @@ public class Game {
     public Hand getPlayer2Hand() {
         return player2;
     }
+
+    public void rankHand(Hand hand) {
+        var listCards = hand.getHand();
+        if (isStraightFlush(listCards)) {
+            hand.setHandRank(HandRank.STRAIGHT_FLUSH);
+        } else if (isFourOfAKind(listCards)) {
+            hand.setHandRank(HandRank.FOUR_OF_A_KIND);
+        }
+    }
+
+    private boolean isStraightFlush(List<Card> listCards) {
+        var currSequence = listCards.get(0).getFaceValue().getValue();
+        var startingSuit = listCards.get(0).getSuitValue();
+        for (int i = 1; i < listCards.size(); i++) {
+            Card currentCard = listCards.get(i);
+            if (currentCard.getSuitValue() != startingSuit) {
+                return false;
+            }
+            int currentCardFaceValue = currentCard.getFaceValue().getValue();
+            if (currentCardFaceValue != (currSequence + 1)) {
+                return false;
+            }
+            currSequence += 1;
+        }
+        return true;
+    }
+
+    private boolean isFourOfAKind(List<Card> listCards) {
+        final int FOUR_OF_A_KIND = 4;
+        var faceValuesList = listCards.stream()
+                .map(Card::getFaceValue)
+                .toList();
+        return faceValuesList.stream()
+                .anyMatch(i -> Collections.frequency(faceValuesList, i) == FOUR_OF_A_KIND);
+    }
+
 }
--- src/test/java/org/alan/GameTest.java
+++ src/test/java/org/alan/GameTest.java
@@ -30,6 +30,41 @@ class GameTest {
         Game game = new Game(player1, player2);
         assertEquals(player1.getHand(), game.getPlayer1Hand().getHand());
         assertEquals(player2.getHand(), game.getPlayer2Hand().getHand());
+    }
 
+    @Test
+    void testPlayersHandHasStraightFlush() {
+        var straightFlush = List.of(
+                new Card(FaceValues.FIVE, SuitValues.SPADES),
+                new Card(FaceValues.SIX, SuitValues.SPADES),
+                new Card(FaceValues.EIGHT, SuitValues.SPADES),
+                new Card(FaceValues.SEVEN, SuitValues.SPADES),
+                new Card(FaceValues.FOUR, SuitValues.SPADES)
+        );
+        Hand player1 = new Hand();
+        player1.setHand(straightFlush);
+        Hand player2 = new Hand();
+        player2.setHand(straightFlush);
+        Game game = new Game(player1, player2);
+        game.rankHand(player1);
+        assertEquals(HandRank.STRAIGHT_FLUSH, player1.getHandRank());
+    }
+
+    @Test
+    void testPlayersHandHasFourOfAKindFlush() {
+        var fourKind = List.of(
+                new Card(FaceValues.KING, SuitValues.SPADES),
+                new Card(FaceValues.KING, SuitValues.DIAMOND),
+                new Card(FaceValues.KING, SuitValues.DIAMOND),
+                new Card(FaceValues.KING, SuitValues.CLUB),
+                new Card(FaceValues.FIVE, SuitValues.HEARTS)
+        );
+        Hand player1 = new Hand();
+        player1.setHand(fourKind);
+        Hand player2 = new Hand();
+        player2.setHand(fourKind);
+        Game game = new Game(player1, player2);
+        game.rankHand(player1);
+        assertEquals(HandRank.FOUR_OF_A_KIND, player1.getHandRank());
     }
 }
